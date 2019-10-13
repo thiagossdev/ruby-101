c923692cb24e0d130384649c391c23a99cf70b02
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2019-10-13 15:44:30 -0300

    ADDED: write module_01/section_06

---
+++ module_01/section_06/hash.rb
@@ -0,0 +1,14 @@
+#hash
+hash = { 'nome' => 'Jhon', 'idade' => 27 }
+puts hash['nome']
+puts hash['idade']
+
+#symbols
+cor1 = :vermelho
+cor2 = :vermelho
+
+#hash with symbols
+hash = { nome: 'Jhon', idade: 27 }
+
+puts hash[:nome]
+puts hash[:idade]
