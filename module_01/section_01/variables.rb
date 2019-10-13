6aab762f199eb20cad04ca35808ee7e2be2ec4cd
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2019-10-13 14:17:25 -0300

    ADDED: write module_01/section_01

---
+++ module_01/section_01/variables.rb
@@ -0,0 +1,19 @@
+#declare local, instance, global and a constant
+ 
+#declare int
+num = 5 #local variable
+ 
+#declare float
+@num2 = 3.5 #instance variable
+ 
+#declare string
+cor = 'verde'
+ 
+#declare boolean
+$teste = true #global variable
+ 
+#declare nil
+@teste2 = nil
+ 
+#declare array
+TESTE = [1, 2, 3] #constant
